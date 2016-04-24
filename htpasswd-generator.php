c97695c2e220beb3d28a2bc3620f060b621b600e
andrea.como <andrea.como@gmail.com>
2016-04-24 11:48:04 +0200

    Added sync on password change. Now .htpasswd is named .htpasswd_generated and is located in plugin folder

--- htpasswd-generator.php
+++ htpasswd-generator.php
@@ -10,11 +10,12 @@ Text Domain: Security
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-add_action( 'user_register', 'cnj_htpasswd_generator_add', 10, 1 );
-add_action( 'profile_update', 'cnj_htpasswd_generator_add', 10, 2 );
-add_action( 'delete_user', 'cnj_htpasswd_generator_remove', 10, 1 );
+add_action( 'user_register', 'cnj_htpasswd_generator_on_add', 10, 1 );
+add_action( 'profile_update', 'cnj_htpasswd_generator_on_add', 10, 2 );
+add_action( 'delete_user', 'cnj_htpasswd_generator_on_remove', 10, 1 );
+add_action( 'password_reset', 'cnj_htpasswd_generator_on_reset', 10, 2 );
 
-function cnj_htpasswd_generator_add($user_id, $user_data) {
+function cnj_htpasswd_generator_on_add($user_id, $user_data) {
     if ($user_data == null) {
         $username = $_POST['user_login'];
     } else {
@@ -25,18 +26,22 @@ function cnj_htpasswd_generator_add($user_id, $user_data) {
     }
 }
 
-function cnj_htpasswd_generator_remove($user_id) {
+function cnj_htpasswd_generator_on_remove($user_id) {
     $user_data = get_userdata( $user_id );
     update_htpasswd($user_data->user_login);
 }
 
+function cnj_htpasswd_generator_on_reset($user, $password) {
+    update_htpasswd($user->user_login, $password);
+}
+
 function update_htpasswd( $username, $password ) {
-    $file = ".htpasswd";
+    $file = plugin_dir_path(__FILE__) . ".htpasswd_generated";
     if (!file_exists($file)) {
         touch($file);
     }
     
-    $passwdFile = fopen($file, "r+") or die("Unable to open file!");
+    $passwdFile = fopen($file, "r+") or die("Unable to open file " . $file);
     $newContent = '';
     while(!feof($passwdFile)) {
 
